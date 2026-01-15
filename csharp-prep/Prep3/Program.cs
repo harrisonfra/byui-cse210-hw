42fcf7a78f0d6fe5e2606bc5365f9fbbd24db2e3
Harrison Fraser <Harrison.f177@gmail.com>
2026-01-14 18:32:31 -0700

    Wrote random number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //string magicNum = Console.ReadLine();
+        //int magicNumberInt = int.Parse(magicNum);
+        Random randomGenerator = new Random();
+        int magicNumberInt = randomGenerator.Next(1, 101);
+        int guessInt = 999;
+        while (guessInt != magicNumberInt)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessInt = int.Parse(guess);
+
+            if (guessInt > magicNumberInt)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessInt < magicNumberInt)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
     }
 }
