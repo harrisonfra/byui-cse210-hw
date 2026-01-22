0c822668a8310c8f4ab3b79d894462b62ed16dc4
Harrison Fraser <Harrison.f177@gmail.com>
2026-01-21 18:55:39 -0700

    Completed code for abstraction learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Crowdstrike";
+        job1._startYear = 2021;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Electrical Engineer";
+        job2._company = "General Electric";
+        job2._startYear = 2024;
+        job2._endYear = 2026;
+        
+        Resume resume1 = new Resume();
+        resume1._name = "John";
+
+        //job1.Display();
+        //job2.Display();
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+
     }
 }
