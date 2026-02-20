74a6a4566f9a905a6cb5d839599e7b6f51d55352
Harrison Fraser <Harrison.f177@gmail.com>
2026-02-19 20:48:11 -0700

    Learning04 Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Josh Justice", "Homework");
+        Console.WriteLine(assignment.GetSummary());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Aaron Fullmer", "History of Cellular Models", "Biology");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+        MathAssignment mathAssignment = new MathAssignment("Aidan Sanchez", "Multiplication", "Section 10", "Problems 10-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
